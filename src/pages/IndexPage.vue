0b224ac8f9f585dee6b3a7583417ecca10f0c116
pohonakasia <yin.sx1990@gmail.com>
2024-03-01 09:33:25 +0700

    update landingpage

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,35 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <head>
+    <title>Landing Page Laundry</title>
+    <link rel="stylesheet" href="src/css/styles.css" />
+  </head>
+
+  <body>
+    <nav class="container">
+      <span class="logo">Logo</span>
+      <ul class="list-items">
+        <li class="list-item">About Us</li>
+        <li class="list-item">Services</li>
+        <li class="list-item">Harga</li>
+        <li class="list-item">Login</li>
+        <li class="list-item">Register</li>
+        <li>
+          <span class="phone-svg"></span>
+          Call Us
+          <br />
+          9373105031
+        </li>
+      </ul>
+    </nav>
+    <main>
+      <div class="hero-section">
+        <h1>Laundry Camelia</h1>
+        <button class="btn">Learn More</button>
+      </div>
+      
+    </main>
+
+  </body>
 </template>
 
 <script>
